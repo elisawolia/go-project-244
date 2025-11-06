d2a606a41725c319510dc859c4ee3434bd41cb90
Elizaveta Volyanitsa <epvolyanitsa@avito.ru>
2025-11-06 16:22:40 +0300

    P-2. Format flag is added

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -12,6 +12,14 @@ func main() {
 		Name:      "gendiff",
 		Usage:     "Compares two configuration files and shows a difference.",
 		UsageText: "gendiff [global options]",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "format",
+				Aliases:     []string{"f"},
+				DefaultText: "stylish",
+				Usage:       "output formatoutput format",
+			},
+		},
 	}
 
 	err := cmd.Run(context.Background(), os.Args)
